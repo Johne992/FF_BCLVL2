3285fc88ee4d95d3800d0e1bd8e3d466e699b8fa
John Lewis <johnelewis92@gmail.com>
2022-03-31 16:37:01 +0000

    Initial commit

---
+++ Nicole.js
@@ -0,0 +1,13 @@
+nickarray = [''n,'i','c','k'];
+
+let n = 0;
+
+while(n< 4){ 
+  nickarray.pop();
+  console.log(nickarray);
+  
+  //console.log('I is less than 4 ');
+  //console.log(i < 4);
+  n++
+  //console.log('I is equal to ' + i);
+}
